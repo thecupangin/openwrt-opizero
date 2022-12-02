8ec273b49860f03119f5c110727b0082b98d6d5e
thecupangin <thecupangin@gmail.com>
2022-12-02 02:31:57 +0000

    add new config for opiz+openclash

---
+++ opiz-part1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# update feeds
+./scripts/feeds update -a
+
+# add themes luci-theme-argon( from jerrykuku)
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+# install feeds
+./scripts/feeds install -a
